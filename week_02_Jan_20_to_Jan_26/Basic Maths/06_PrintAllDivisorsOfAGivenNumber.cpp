305d15dcf0dad420dd67a52449cfd76f43f94f6c
J-Man-2024 <jeeteshmandal@gmail.com>
2025-01-26 11:52:22 +0530

    basic maths problem 6 solved

---
+++ week_02_Jan_20_to_Jan_26/Basic Maths/06_PrintAllDivisorsOfAGivenNumber.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+// Brute force method
+void divisorsOfANumBruteForce(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+}
+
+// Optimal approach
+void divisorsOfANumOptimized(int n)
+{
+    for (int i = 1; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            cout << i << " ";
+        }
+        else if (i != n / i)
+        {
+            cout << n / i << " ";
+        }
+    }
+    cout << endl;
+}
+int main()
+{
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+    divisorsOfANumBruteForce(n);
+    return 0;
+}
