e27a37e2b25716032c741334cff49cd6a876a0d5
J-Man-2024 <jeeteshmandal@gmail.com>
2025-02-01 16:07:51 +0530

    Important sorting techniques - Bubble Sort done

---
+++ week_03_Jan_27_to_Feb_02/Important Sorting Techniques/02_BubbleSort.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+void printArr(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void bubbleSort(int arr[], int n)
+{
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        bool swapped = false;
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if (!swapped)
+            break;
+    }
+}
+int main()
+{
+    int arr[] = {13, 46, 24, 52, 20, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    bubbleSort(arr, n);
+    printArr(arr, n);
+    return 0;
+}
