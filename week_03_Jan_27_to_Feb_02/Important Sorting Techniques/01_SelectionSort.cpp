74c70b7f9204a04d9eacc00bc089e181cac84270
J-Man-2024 <jeeteshmandal@gmail.com>
2025-02-01 12:13:27 +0530

    Important sorting techniques - Selection Sort done

---
+++ week_03_Jan_27_to_Feb_02/Important Sorting Techniques/01_SelectionSort.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+// print array
+void printArr(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+// Selection sorting using arrays
+void selectionSortArr(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int min = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[min] > arr[j])
+            {
+                min = j;
+            }
+        }
+        if (i != min)
+        {
+            swap(arr[i], arr[min]);
+        }
+    }
+}
+
+int main()
+{
+    int arr[] = {13, 46, 24, 52, 20, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    selectionSortArr(arr, n);
+    printArr(arr, n);
+    return 0;
+}
