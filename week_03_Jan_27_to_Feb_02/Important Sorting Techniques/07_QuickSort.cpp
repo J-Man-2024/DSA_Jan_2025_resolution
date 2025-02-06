a1226284f8b6afaa2ce30ac5610f86421a8f300c
J-Man-2024 <jeeteshmandal@gmail.com>
2025-02-06 19:38:05 +0530

    Important sorting techniques - Quick sort completed

---
+++ week_03_Jan_27_to_Feb_02/Important Sorting Techniques/07_QuickSort.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int partition(vector<int> &arr, int low, int high)
+{
+    int pivot = arr[low];
+    int i = low, j = high;
+
+    while (i < j)
+    {
+        while (arr[i] <= pivot && i <= high - 1)
+        {
+            i++;
+        }
+        while (arr[j] > pivot && j >= low + 1)
+        {
+            j--;
+        }
+        if (i < j)
+            swap(arr[i], arr[j]);
+    }
+    swap(arr[low], arr[j]);
+
+    return j;
+}
+void quickSort(vector<int> &arr, int low, int high)
+{
+    if (low < high)
+    {
+
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main()
+{
+    vector<int> arr = {4, 1, 7, 9, 3};
+    int n = arr.size();
+
+    quickSort(arr, 0, n - 1);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
