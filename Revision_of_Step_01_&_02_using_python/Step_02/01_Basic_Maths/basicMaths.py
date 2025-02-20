95032e668415eb8421e19d96f37498bc17ffec51
J-Man-2024 <jeeteshmandal@gmail.com>
2025-02-20 19:29:12 +0530

    Repeating Revision of step-02 Basic maths solved till problem 3

--- Revision_of_Step_01_&_02_using_python/Step_02/01_Basic_Maths/basicMaths.py
+++ Revision_of_Step_01_&_02_using_python/Step_02/01_Basic_Maths/basicMaths.py
@@ -2,7 +2,7 @@ import math
 
 def problem01BruteForce(n):
     count = 0
-    while(n):
+    while n:
         count+=1
         n = n // 10
     return count
@@ -11,13 +11,30 @@ def problem01Optimized(n):
     count = int(math.log10(n)+1)
     return count
     
+def problem02(n):
+    revNum = 0
+    while n:
+        lastDigit = n % 10
+        revNum = revNum * 10 + lastDigit
+        n = n // 10
+    return revNum
+
+def problem03(n):
+    cpy = n
+    revNum = 0
     
+    while n:
+        lastDigit = n % 10
+        revNum = revNum * 10 + lastDigit
+        n = n // 10
+    if cpy == revNum:
+        return True
+    else:
+        return False
 ##################
 def main():
-    n = 12345 
-    ans = problem01Optimized(n)
-    print(f"The number of digits in \"{n}\" is : {ans}.")
-
+    n = 4554
+    print(problem03(n))
 
 if __name__ == "__main__":
     main()
