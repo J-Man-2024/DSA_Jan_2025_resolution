b480fe2b3d3a1912a6578c647cb4361d8ba4771b
J-Man-2024 <jeeteshmandal@gmail.com>
2025-03-14 11:36:14 +0530

    Revision of step-02 basic of sorting started

---
+++ Revision_of_Step_01_&_02_using_python/Step_02/02_Bubble_Sort/02_Bubble_Sort/bubble_sort.py
@@ -0,0 +1,11 @@
+arr = [5, 4, 3, 2, 1]
+n = len(arr)
+for i in range(n - 1):
+    swapped = False
+    for j in range(n - i - 1):
+        if arr[j] > arr[j + 1]:
+            arr[j], arr[j + 1] = arr[j + 1], arr[j]
+            swapped = True
+    if not swapped:
+        break
+print(arr)
