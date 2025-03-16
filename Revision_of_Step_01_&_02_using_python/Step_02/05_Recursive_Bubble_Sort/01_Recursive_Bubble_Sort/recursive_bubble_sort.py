24e8eac17051593b398b4450c94cf987706c9d40
J-Man-2024 <jeeteshmandal@gmail.com>
2025-03-16 16:22:12 +0530

    Revision of step-02 basic of sorting started

---
+++ Revision_of_Step_01_&_02_using_python/Step_02/05_Recursive_Bubble_Sort/01_Recursive_Bubble_Sort/recursive_bubble_sort.py
@@ -0,0 +1,16 @@
+def bubble_sort(arr, n):
+    if n == 1:
+        return
+    for i in range(n - 1):
+        if arr[i] > arr[i + 1]:
+            arr[i], arr[i + 1] = arr[i + 1], arr[i]
+    bubble_sort(arr, n - 1)
+###############
+def main():
+    arr = [13,46,24,52,20,9]
+    n = len(arr)
+    bubble_sort(arr, n)
+    print(arr)
+###############
+if __name__ == "__main__":
+    main()
