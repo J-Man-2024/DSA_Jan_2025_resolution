d3ea6a800c6c611a244a0e8a574d8e2893bbc17a
J-Man-2024 <jeeteshmandal@gmail.com>
2025-03-26 19:34:10 +0530

    Step 03 Easy problems started

--- week_04_Mar_20_to_Mar_27/01_Easy/problem_06.py
+++ week_04_Mar_20_to_Mar_27/01_Easy/problem_06.py
@@ -19,11 +19,35 @@ def rotate_left_or_right_d_places_brute(arr, rotate, d):
     else:
         raise ValueError("Rotation value must be either 'right' or 'left'.")
     return temp_array
+# It takes O(n+n) or simply O(n) time and O(n) extra space for temp array.
+
+def reverse_array(arr, start, end):
+    while start < end:
+        arr[start], arr[end] = arr[end], arr[start]
+        start += 1
+        end -= 1
+def rotate_left_or_right_d_places_optimal(arr, rotate, d):
+    n = len(arr)
+    if n < 2 or d == 0:
+        return arr
+    d = d % n
+    if rotate == "right":
+        reverse_array(arr, 0, n - d - 1)
+        reverse_array(arr, n - d, n - 1)
+        reverse_array(arr, 0, n - 1)
+    elif rotate == "left":
+        reverse_array(arr, 0, d - 1)
+        reverse_array(arr, d, n - 1)
+        reverse_array(arr, 0, n - 1)
+    else:
+        raise ValueError("Rotation value must be either 'right' or 'left'.")
+    return arr
+
 def main():
     arr = [1, 2, 3, 4, 5, 6, 7]
-    d = 2
+    d = 1
     rotate = "left"
-    rotated_array = rotate_left_or_right_d_places_brute(arr,rotate, d)
+    rotated_array = rotate_left_or_right_d_places_optimal(arr,rotate, d)
     print(rotated_array)
 if __name__ == "__main__":
     main()
