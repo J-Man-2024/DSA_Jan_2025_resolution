23c2ebca081f28f230c91c2aeb2858941c55f7a3
J-Man-2024 <jeeteshmandal@gmail.com>
2025-04-04 15:40:40 +0530

    Step 03 Easy problems started

--- week_04_Mar_20_to_Mar_27/01_Easy/problem_13.py
+++ week_04_Mar_20_to_Mar_27/01_Easy/problem_13.py
@@ -12,10 +12,23 @@ def sum_of_max_sub_arr_brute(arr, k):
     return max_len
 # It takes O(n^2) and O(1) extra space
 
+def sum_of_max_sub_arr_better(arr, k):
+    prefix_sum = 0
+    max_len = 0
+    prefix_map = {}
+
+    for i in range(len(arr)):
+        prefix_sum += arr[i]
+        
+        if prefix_sum - k in prefix_map:
+            max_len = max(max_len, i - prefix_map[prefix_sum - k])
+        if prefix_sum not in prefix_map:
+            prefix_map[prefix_sum] = i
+    return max_len
 def main():
     arr = [9, 1, 5, 3, 2]
     k = 10
-    solution = sum_of_max_sub_arr_brute(arr, k)
+    solution = sum_of_max_sub_arr_better(arr, k)
     print(solution)
 
 if __name__ == "__main__":
