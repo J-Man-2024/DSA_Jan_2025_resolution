bb4ae0ddcf66b2a4259b0c780a8c6e97dc6799d9
J-Man-2024 <jeeteshmandal@gmail.com>
2025-04-05 19:04:08 +0530

    Step 03 Easy problems started

--- week_04_Mar_20_to_Mar_27/01_Easy/problem_13.py
+++ week_04_Mar_20_to_Mar_27/01_Easy/problem_13.py
@@ -24,10 +24,24 @@ def sum_of_max_sub_arr_better(arr, k):
         if prefix_sum not in prefix_hashmap:
             prefix_hashmap[prefix_sum] = i
     return max_len
+# It takes O(n) and O(n) extra space due to hashmap
+
+def sum_of_max_sub_arr_best(arr, k):
+    left = 0
+    curr_sum = 0
+    max_len = 0
+    for right in range(len(arr)):
+        curr_sum += arr[right]
+        while curr_sum > k and left <= right:
+            curr_sum -= arr[left]
+            left += 1
+        if curr_sum == k:
+            max_len = max(max_len, right - left + 1)
+    return max_len
 def main():
     arr = [9, 1, 5, 3, 2]
     k = 10
-    solution = sum_of_max_sub_arr_better(arr, k)
+    solution = sum_of_max_sub_arr_best(arr, k)
     print(solution)
 
 if __name__ == "__main__":
