92db17cb6dcde9100752158f51c6d3118b755c93
J-Man-2024 <jeeteshmandal@gmail.com>
2025-04-13 11:37:24 +0530

    Revision number 2 started till array easy questions

--- week_05_Apr_09_to_Apr_16/Revision_02/02_Basic_Maths/basic_maths.py
+++ week_05_Apr_09_to_Apr_16/Revision_02/02_Basic_Maths/basic_maths.py
@@ -33,16 +33,36 @@ def palindrome_or_not(n):
 
 def gcd_brute(n1, n2):
     n = min(n1, n2)
-    gcd = []
+    gcd = 1
     for i in range(1, n + 1):
         if n1 % i == 0 and n2 % i == 0:
-            gcd.append(i)
-    return max(gcd)    
+            gcd = i
+    return gcd
+# It takes O(min(n1,n2)) and O(1) extra space
 
+def gcd_better(n1, n2):
+    n = min(n1, n2)
+    for i in range(n, 0, -1):
+        if n1 % i == 0 and n2 % i == 0:
+            return i
+    return 1
+# It takes O(min(n1, n2)) and O(1) extra space but has fewer iteration for the worst case.
+
+def gcd_optimal(n1, n2):
+    while n1 > 0 and n2 > 0:
+        if n1 > n2:
+            n1 %= n2
+        else:
+            n2 %= n1
+    if n1 == 0:
+        return n2
+    return n1
+
+# It takes O(min(n1, n2)) time and O(1) extra space
 def main():
     n1 = 9
     n2 = 12
-    print(gcd_brute(n1, n2))
+    print(gcd_optimal(n1, n2))
 
 if __name__ == "__main__":
     main()
