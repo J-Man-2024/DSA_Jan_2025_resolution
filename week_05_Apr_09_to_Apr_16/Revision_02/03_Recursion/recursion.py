41145aa383fefa1e9b9c11c5677478f64d58bd53
J-Man-2024 <jeeteshmandal@gmail.com>
2025-04-19 15:00:22 +0530

    Revision number 2 started till array easy questions

--- week_05_Apr_09_to_Apr_16/Revision_02/03_Recursion/recursion.py
+++ week_05_Apr_09_to_Apr_16/Revision_02/03_Recursion/recursion.py
@@ -25,11 +25,18 @@ def reverse_arr(arr, start, end):
     if start < end:
         arr[start], arr[end] = arr[end], arr[start]
         reverse_arr(arr, start + 1, end - 1)
+
+def palindrome_or_not_string(arr, start, end):
+    if start >= end:
+        return True
+    if arr[start] != arr[end]:
+        return False
+    return palindrome_or_not_string(arr, start + 1, end - 1)
 def main():
-    arr = [1,2,3,4,5]
+    arr = "ABCDEBA"
     n = len(arr)
-    reverse_arr(arr, 0, n - 1)
-    print(arr)
+    print(palindrome_or_not_string(arr, 0, n - 1))
+    
 
 if __name__ == "__main__":
     main()
