1c8c137aa803c739aa97e8929a2c5508baedd253
J-Man-2024 <jeeteshmandal@gmail.com>
2025-04-23 18:43:03 +0530

    Revision number 2 started till array easy questions

--- week_05_Apr_09_to_Apr_16/Revision_02/04_Hashing/hashing.py
+++ week_05_Apr_09_to_Apr_16/Revision_02/04_Hashing/hashing.py
@@ -1,6 +1,6 @@
 from collections import Counter
 from collections import defaultdict
-
+import math
 
 def freq_count_brute(arr):
     n = len(arr)
@@ -14,7 +14,7 @@ def freq_count_brute(arr):
                 count += 1
                 visited[j] = True
         print(f"{arr[i]} appears : {count} times.")
-# It takes O(n^2) time and O(1) extra space
+# It takes O(n^2) time and O(n) extra space
 def freq_count_better(arr):
     freq = {}
     for num in arr:
@@ -34,8 +34,53 @@ def freq_count(arr):
     for key, value in freq.items():
         print(f"{key} appears : {value} times.")
 # It takes time : O(n) and O(n) extra space
+
+def high_or_low_brute(arr):
+    n = len(arr)
+    freq = [False] * n
+    max_freq = -1
+    max_value = None
+    min_value = None
+    min_freq = n
+    for i in range(n):
+        if freq[i] == True:
+            continue
+        count = 0
+        for j in range(n):
+            if arr[i] == arr[j]:
+                count += 1
+                freq[j] = True
+        if count > max_freq:
+            max_freq = count
+            max_value = arr[i]
+        elif count < min_freq:
+            min_freq = count
+            min_value = arr[i]
+    print(f"Max =>{max_value} appears : {max_freq}\nMin =>{min_value} appears : {min_freq}")
+# It takes O(n^2) times and O(n) extra space
+
+def high_or_low(arr):
+    n = len(arr)
+    freq = {}
+    high_freq = 0
+    low_freq = n
+    high_freq_value = None
+    low_freq_value = None
+    for num in arr:
+        freq[num] = freq.get(num, 0) + 1
+
+    for key, value in freq.items():
+        if value > high_freq:
+            high_freq = value
+            high_freq_value = key
+        elif value < low_freq:
+            low_freq = value
+            low_freq_value = key
+    print(f"High Freq => {high_freq_value} appears : {high_freq} times.\nLow Freq => {low_freq_value} appears : {low_freq} times.")
+# It takes O(n+n) and O(n) extra space
+
 def main():
     arr = [10, 5, 10, 15, 10, 5]
-    freq_count(arr)
+    high_or_low(arr)
 if __name__ == "__main__":
     main()
