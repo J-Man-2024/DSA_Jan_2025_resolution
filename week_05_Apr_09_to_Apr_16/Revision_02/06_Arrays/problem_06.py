f0d678086cc88480022bc652f620955bccc11209
J-Man-2024 <jeeteshmandal@gmail.com>
2025-06-04 13:50:11 +0530

    Revision number 2 started till array easy questions

--- week_05_Apr_09_to_Apr_16/Revision_02/06_Arrays/problem_06.py
+++ week_05_Apr_09_to_Apr_16/Revision_02/06_Arrays/problem_06.py
@@ -1,10 +1,8 @@
-def rotate_array_brute(arr, k, direction):
+def rotate_arr_brute(arr, k, direction):
     n = len(arr)
     if n == 0:
-        return
+        return arr
     rotation = k % n
-    if rotation > n:
-        return 
     temp_arr = [0] * n
     if direction == 'right':
         for i in range(n - rotation, n):
@@ -18,10 +16,32 @@ def rotate_array_brute(arr, k, direction):
             temp_arr[n - rotation + i] = arr[i]
     return temp_arr
 # It takes O(n) time and O(n) space
+def arr_rotation(arr, start, end):
+    n = len(arr)
+    while start <= end:
+        arr[start], arr[end] = arr[end], arr[start]
+        start += 1
+        end -= 1
+    return arr
+def rotate_arr_optimal(arr, k, direction):
+    n = len(arr)
+    if n == 0:
+        return arr
+    rotation = k % n
+    if direction == 'right':
+        arr_rotation(arr, n - rotation, n - 1)
+        arr_rotation(arr, 0, n - rotation - 1)
+        arr_rotation(arr, 0, n - 1)
+    elif direction == 'left':
+        arr_rotation(arr, 0, rotation - 1)
+        arr_rotation(arr, rotation, n - 1)
+        arr_rotation(arr, 0, n - 1)
+    return arr
+# It takes O(n) time and O(1) extra space
 def main():
     arr = [1,2,3,4,5,6,7]
-    k = 1
+    k = 2
     direction = 'left'
-    print(rotate_array_brute(arr, k, direction))
+    print(rotate_arr_optimal(arr, k, direction))
 if __name__ == "__main__":
     main()
